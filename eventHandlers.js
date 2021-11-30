569c532e21f00d2520be418b92d54dc49b92b2ba
PhilippeFuhrer <71648397+PhilippeFuhrer@users.noreply.github.com>
2021-11-30 22:53:20 +0100

    changes 30.11

--- eventHandlers.js
+++ eventHandlers.js
@@ -7,6 +7,7 @@ exports.registerEventHandlers = function (source) {
     source.addEventListener('Temperatur', handleTemperature);
     source.addEventListener('Luftfeuchtigkeit', handleLuftfeuchtigkeit);
     source.addEventListener('Luftdruck', handleLuftdruck);
+    source.addEventListener('KissenRein', handleKissen);
 }
 
 function handleMyEvent(event) {
@@ -92,5 +93,26 @@ function handleLuftdruck(event) {
         console.log(error)
     }
 }
+function handleKissen(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
 
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+        // Log the event in the database
+        logger.logOne("MyDB", "KissenRein", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
 
